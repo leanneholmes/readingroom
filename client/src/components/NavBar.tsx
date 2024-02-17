ffa6edaec5fad474851442781bce6a597b1d25dd
Leanne Holmes <lholmes24@my.bcit.ca>
2024-02-16 18:58:19 -0800

    Delete book club, navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { Menu, MenuMenu } from "semantic-ui-react";
 
 export default function NavBar() {
@@ -6,9 +7,9 @@ export default function NavBar() {
       <MenuMenu position="left">
         <Menu.Item header>Reading Room</Menu.Item>
       </MenuMenu>
-      <Menu.Item name="Browse for Book Clubs"></Menu.Item>
-      <Menu.Item name="Create a Club"></Menu.Item>
-      <Menu.Item name="My Clubs"></Menu.Item>
+      <Menu.Item as={NavLink} to="/" name="Browse for Book Clubs"></Menu.Item>
+      <Menu.Item as={NavLink} to="/create" name="Create a Club"></Menu.Item>
+      <Menu.Item as={NavLink} to="/myclubs" name="My Clubs"></Menu.Item>
       <MenuMenu position="right">
         <Menu.Item name="Logout"></Menu.Item>
       </MenuMenu>
